97c965d4ae5a1e15e4d04b6505a00a6c1295fc48
Jack Huang <51463712+flpxzglgh@users.noreply.github.com>
2019-07-08 17:10:03 -0500

    Update assinment.cpp

--- assinment.cpp
+++ assinment.cpp
@@ -1,224 +1,198 @@
 #include <iostream>
-
 using namespace std;
 
-template <class EType>
-class Node
-{
-	public:
-    EType data;
-    Node * next;
-    Node():next(NULL){}
-};
-
-template <class ElementType>
-class List_Linked
-{
+class Node{
 public:
-
-	List_Linked(): mySize(0)
-	{
-	}
-
-
-    /***** empty operation *****/
-	bool empty() const
-	{
-		return mySize == 0;
-	}
-
-
-    /***** insert and erase *****/
-
-
-	void insert_before_node(ElementType item){
-		Node<ElementType> * newnode = new Node<ElementType>;
-		newnode->data = item;
-		Node<ElementType> * curnode = first;
-				int curind = 0;
-			
-				while(curnode != NULL && curind < pos-1)
-				{
-					curnode = curnode->next;
-					curind ++;
-				}
-			
-				newnode->next = curnode->next;
-				curnode->next = newnode;
-				mySize++;
-
-	}
-	void insert_after_node(ElementType item){
-
-	}
-
-void delete_before_node(int pos){
-
-}
-void delete_after_node(int pos){
-
-}
-
-
-	void insert(ElementType item, int pos)
-	{
-		if (!is_pos_legal(pos,true))
-		{
-			return;
-		}
-
-		Node<ElementType> * newnode = new Node<ElementType>;
-		newnode->data = item;
-
-		if (empty())
-			first = newnode;
-		else
-		{
-			if(pos == 0)
-			{
-				newnode->next = first;
-				first = newnode;
-			}
-			else if(pos == mySize)
-			{
-				Node<ElementType> * curnode = first;
-			
-				while(curnode->next != NULL)
-				{
-					curnode = curnode->next;
-				}
-				curnode->next = newnode;
-			}
-			else
-			{
-				Node<ElementType> * curnode = first;
-				int curind = 0;
-			
-				while(curnode != NULL && curind < pos-1)
-				{
-					curnode = curnode->next;
-					curind ++;
-				}
-			
-				newnode->next = curnode->next;
-				curnode->next = newnode;
-			}        
-		}
-		mySize++;
-	}
-
-	void erase(int pos)
-	{
-		if (!is_pos_legal(pos))
-		{
-			return;
-		}
-	
-		if(pos == 0)
-		{
-			Node<ElementType> * temp = first;
-			first = first->next;
-			delete temp;
-		}
-		else if(pos == mySize-1)
-		{
-			Node<ElementType> * curnode = first;
-			while(curnode->next->next != NULL)
-			{
-				curnode = curnode->next;
-			}
-			Node<ElementType> * temp = curnode->next;
-			curnode->next = NULL;
-			delete temp;
-		}
-		else
-		{
-			Node<ElementType> * curnode = first;
-			int curind = 0;
-			while(curnode != NULL && curind < pos-1)
-			{
-				curnode = curnode->next;
-				curind++;
-			}
-			Node<ElementType> * temp = curnode->next;
-			curnode->next = temp->next;
-			delete temp;
-		}
-		mySize--;
-	}    
-	
-    /***** output *****/
-	void display(ostream & out) const
-	{
-		Node<ElementType> * temp = first;
-		while(temp != NULL)
-		{
-			out << temp->data << " | ";    	
-			temp = temp->next;
-		}
-	}    
-    /***** access to existing element  *****/    
-	bool is_pos_legal(int pos,bool insert=false)
-	{
-		if(insert)
-		{
-			if (pos < 0 || pos > mySize)
-			{
-				cerr << "Illegal location to insert -- " << pos
-				<< ".  List_Linked unchanged. ***\n";
-				return false;
-			}
-			else
-				return true;
-		}
-		else
-		{
-			if (pos < 0 || pos >= mySize)
-			{
-				cerr << "Illegal location to erase -- " << pos
-				<< ".  List_Linked unchanged. ***\n";
-				return false;
-			}
-			else
-				return true;        
-		}
-	}
-    
-private:
-    int mySize;                     // current size of List_Linked stored
-    Node<ElementType> * first;
+	Node* next;
+	int data;
 };
 
-//--- Definition of output operator
-template <class ElementType>
-ostream & operator<< (ostream & out, const List_Linked<ElementType> & aList_Linked)
-{
-    aList_Linked.display(out);
-    return out;
+class Linkedlist{
+public:
+	int size;//the list size;
+	Node* head;
+	Linkedlist();
+	~Linkedlist();
+	void insert_at_begining(int data);
+	void insert_at_end(int data);
+	void insert_at_position(int,int);
+	void delete_at_position(int);
+	void delete_at_begining();
+    void delete_at_end();
+
+	void display();
+};
+ 
+Linkedlist::Linkedlist(){
+	this->size=0;
+	this->head=NULL;
 }
 
+Linkedlist::~Linkedlist(){
+	cout<<"LIST DELETED";
+}
 
+void Linkedlist::insert_at_begining(int data){
+	Node*node=new Node();
+	node->data=data;
+	node->next=this->head;
+	this->head=node;
+	this->size++;
+}
+ void Linkedlist::insert_at_end(int data){
+ 	Node *p = new Node;
+    Node *r = new Node;
+    Node *q = new Node;
+    r = this->head;
+    p->data = data;
+    p->next = NULL;
+    if(head == NULL){
+        head = p;
+    }else{
+        while(r->next!=NULL){
+            r = r->next;
+        }
+        r->next = p;
+    }
+ }
+
+void Linkedlist::insert_at_position(int value,int position){
+	Node *p = new Node;
+    Node *l = new Node;
+    Node *r = new Node;
+    bool isFound = false;
+    p=head;
+    int count=0;
+    while(p != NULL){
+        if(count == position){
+            isFound = true;
+            break;
+        }
+        count++;
+        l = p;
+        p = p->next;
+
+    }
+    r->data=value;
+    l->next = r;
+    r->next = p;
+    if(isFound==false){
+    	cout<<"Bad position! Enter the data again!"<<endl;
+    	int v,p;
+    	cin>>v>>p;
+    	insert_at_position(v,p);
+    }
+}
+
+void Linkedlist::display(){
+	Node* h = this->head;
+	int i=1;
+	while(h){
+		cout<<i-1<<":"<<h->data<<endl;
+		h=h->next;
+		i++;
+	}
+}
+
+void Linkedlist::delete_at_position(int position){
+	Node *p = new Node;
+    Node *r = new Node;
+    bool isFound = false;
+    p = head;
+    int count=0;
+    while(p!=NULL){
+        if(count == position-1){
+            isFound = true;
+            break;
+        }
+        p = p->next;
+        r=p->next;
+        count++;
+       
+    }
+   		p->next=r->next;
+   		delete r;
+
+
+}
+
+void Linkedlist::delete_at_begining(){
+   Node *p = new Node;
+    if(head == NULL){
+        cout<<"\nList is Empty\n";
+    }else{
+        p = head;
+        head = head->next;
+        delete p;
+    }
+
+}
+
+void Linkedlist::delete_at_end(){
+
+	Node *p = new Node;
+  	Node *l = new Node;
+  if(head == NULL){
+    cout<<"\nList is Empty\n";
+  }else{
+    p = head;
+    if(p->next == NULL){
+        head = NULL;
+        delete p;
+    }else{
+        while(p->next!= NULL){
+            l = p;
+            p = p->next;
+        }
+        l->next = NULL;
+        delete p;
+    }
+  }
+}
 
 int main()
 {
-	cout << "========== list1  =========\n";
-	List_Linked<double> mylist;
-	mylist.insert(5.7,0);
-	mylist.insert(10.4,1);	
-	cout << mylist << endl;
 	
-	cout << "========== list1 erase val  =========\n";	
-	mylist.erase(0);
-	cout << mylist << endl;
+	Linkedlist* list = new Linkedlist();
 
-	cout << "========== list2  =========\n";	
-	List_Linked<double> mylist1 = mylist;
-	mylist1.insert(0.5,0);
-	mylist1.insert(0.2,1);
-	cout << mylist1 << endl;
-	
+/*
+	for (int i = 0; i < 11; ++i)
+   {
+       list->insert_at_begining(i);
+   }
+   list->display();
+   cout<<endl;
+   list->insert_at_position(333,1);
+   list->display();
 
-	
-	
+ cout<<endl;
+ list->delete_at_position(3);
+    list->display();
+
+cout<<endl;
+   list->insert_at_position(999,3);
+   list->display();*/
+
+   for (int i = 0; i < 11; ++i)
+   {
+       list->insert_at_end(i);
+   }
+   list->display();
+
+ cout<<endl;
+ for (int i = 0; i < 3; ++i)
+   {
+       list->delete_at_begining();
+   }
+   list->display();
+ cout<<endl;
+for (int i = 0; i < 3; ++i)
+   {
+       list->delete_at_end();
+   }
+   list->display();
+
+   //std::cout << "List Length: " << list->length << std::endl;
+   delete list;
 	return 0;
 }
